cf137cea9d12645a90eeb0049d75b8573932a63f
Isaac <isaac-dx@live.com.mx>
2019-09-10 17:47:16 -0500

    cuarto commit manejo de mas metodos

--- ExamenRedes/src/examenredes/ExamenRedes.java
+++ ExamenRedes/src/examenredes/ExamenRedes.java
@@ -8,21 +8,17 @@ package examenredes;
 public class ExamenRedes {
 
     public static void main(String[] args) {
-        String[][] matriz= new String[10][10];
-        int i,j,a;
-        for(i=0;i<10;i++){
-            a = (10*i)+1;
-            System.out.println("");
-            for(j=0;j<10;j++){
-                matriz[i][j] = Integer.toString(a);
-                a++;
-                System.out.printf("%3s ",matriz[i][j]);
-            }
-        }
+        int i,j;
+        
+        String[][] matriz2 = creaMatriz(10,10);
+        
+        imprimeMatriz(matriz2);
+        
         System.out.println("\n");
         for(i=0;i<10;i++){
-            for(j=0;j<10;j++)
-                recorreMatriz(matriz,i,j);
+            for(j=0;j<10;j++){
+                recorreMatriz(matriz2,i,j);
+            }
         }
     }
     
@@ -41,7 +37,33 @@ public class ExamenRedes {
         }
         System.out.println("estan alrededor de: "+m[fila][columna]);
     }
-    static void creaMatriz(int filas,int columnas){
-        
+    static String[][] creaMatriz(int filas,int columnas){
+        String[][] matriz= new String[filas][columnas];
+        int i,j,a;
+        for(i=0;i<10;i++){
+            a = (10*i)+1;
+            for(j=0;j<10;j++){
+                matriz[i][j] = Integer.toString(a);
+                a++;
+            }
+        }
+        return matriz;
+    }
+    
+    static void muestraTablero(String[][] m){
+        int i,j;
+        for(i=0;i<m.length;i++){
+            
+        }
+    }
+    
+    static void imprimeMatriz(String[][] m){
+        int i,j;
+        for(i=0;i<m.length;i++){
+            System.out.println("");
+            for(j=0;j<m[i].length;j++){
+                System.out.printf("%3s ",m[i][j]);
+            }
+        }
     }
 }
